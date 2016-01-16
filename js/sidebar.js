4b6125931a3bbefff8fd77690775a1e5ee0e02f1
Wayne Chi <waynchi@gmail.com>
2016-01-15 16:54:35 -0800

    Initial commit

---
+++ js/sidebar.js
@@ -0,0 +1,18 @@
+//Menu Toggle
+/*$("#menu-toggle").click(function(e) {
+	e.preventDefault();
+	$("#wrapper").toggleClass("toggled");
+});*/
+
+/* Page Links */
+(function() {
+	var app = angular.module('portfolio', []);
+	app.controller('portfolioController', function() {
+		this.product = home;
+	});
+	
+	var home = {
+		
+	}
+	
+})();
